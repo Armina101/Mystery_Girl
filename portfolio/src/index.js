d5288b832ec03b70205a1179c4a9e386bb8d564a
Aminat Afolabi <Afolabijolayemi@gmail.com>
2024-07-04 07:53:59 -0700

    Updates

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {HelmetProvider} from "react-helmet-async"
+import { AnimatePresence } from "framer-motion";
+import { Contact, Home, Portfolio } from "./components";
+import App from "./App";
+import "./index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// const location = useLocation();
+
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <BrowserRouter>
+          <AnimatePresence>
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route exact path="/" index element={<Home />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </AnimatePresence>
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
